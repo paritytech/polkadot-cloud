711f9caa0e75dd14a01a6dde31cfd213fa1dcfcf
Ross Bulat <ross@jkrbinvestments.com>
2022-08-27 22:43:39 +0100

    init project

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+
+const packageJson = require("./package.json");
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+ export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: packageJson.main,
+      format: "es",
+      sourcemap: false
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      }, 
+      plugins: [
+        simplevars(),
+        nested(),
+        cssnext({ warnForDuplicates: false, }),
+        cssnano()
+      ],
+      extensions: ['.css'],
+      minimize: true,
+      modules: false,
+      extract: true,
+    }),
+  ]
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export const someComponent = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+      Hello World
+    </button>
+  );
+}
