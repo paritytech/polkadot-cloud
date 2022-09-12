959a502e921b1e8824cb0491bcda78b879a5dc0e
Ross Bulat <ross@jkrbinvestments.com>
2022-09-12 16:22:04 +0700

    add delete plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import del from "rollup-plugin-delete";
 import simplevars from "postcss-simple-vars";
 import nested from "postcss-nested";
 import cssnext from "postcss-cssnext";
@@ -20,11 +21,8 @@ export default {
     },
   ],
   plugins: [
+    del({ targets: "dist/*" }),
     peerDepsExternal(),
-    resolve(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
     postcss({
       config: {
         path: "./postcss.config.js",
@@ -38,7 +36,11 @@ export default {
       extensions: [".css"],
       minimize: true,
       modules: false,
-      extract: true,
+      extract: "index.css",
+    }),
+    resolve(),
+    typescript({
+      useTsconfigDeclarationDir: true,
     }),
   ],
 };
