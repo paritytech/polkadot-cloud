86c66c4bd4ecea79035077f33b6ced1e73b52c3f
Ross Bulat <ross@jkrbinvestments.com>
2022-11-04 13:48:16 +0000

    rn props

--- lib/core/BodyInterface.tsx
+++ lib/core/BodyInterface.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { StyledComponentInterface } from "../types";
 
-export type BodyInterfaceProps = StyledComponentInterface;
+export type Props = StyledComponentInterface;
 
 /**
  * An element that wraps the entire window, houses `SideInterface` and `BodyInterface`.
  */
-export const BodyInterface = ({ style, children }: BodyInterfaceProps) => (
+export const BodyInterface = ({ style, children }: Props) => (
   <div className="box-border flex flex-auto flex-row relative" style={style}>
     {children}
   </div>
