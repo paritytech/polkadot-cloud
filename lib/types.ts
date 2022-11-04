91f44407fa7de41fbfb2d66f0f4a0c250d3a255c
Ross Bulat <ross@jkrbinvestments.com>
2022-11-04 13:47:30 +0000

    lint fixes

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,7 @@
 import React from "react";
 
+// A generic type to handle React components. We assume the component may have
+// children and styling applied to it.
 export interface StyledComponentInterface {
   children?: React.ReactNode;
   style?: React.CSSProperties;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import "../styles/index.css";
+import "../styles/index.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
