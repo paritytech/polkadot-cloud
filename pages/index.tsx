a24efa332b009a603d1a307151ccab926e15b1f2
Ross Bulat <ross@jkrbinvestments.com>
2022-11-05 22:15:03 +0000

    toggle themes in demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { ButtonPrimary } from "../lib/buttons/ButtonPrimary";
@@ -16,100 +16,126 @@ import {
  * Sandbox page for component tests and class inclusion.
  */
 const Home: NextPage = () => {
+  // store the current theme
+  const [theme, setTheme] = useState<string>("light");
+
+  // store the current network
+  const [network, setNetwork] = useState<string>("polkadot");
+
   return (
-    <div>
+    <>
       {/* dump all themes here as to not be purged at build */}
       <div className="theme-polkadot theme-kusama theme-westend theme-light theme-dark"></div>
-      <Head>
-        <title>Polkadot Dashboard UI</title>
-        <meta name="description" content="Polkadot Dashboard UI" />
-      </Head>
-      <main className="p-4 flex flex-col gap-8">
-        <h1 className="text-3xl">Components</h1>
-        <h2 className="text-xl">Button Primary</h2>
+      <div
+        className={`theme-${network} theme-${theme}`}
+        style={{
+          background: theme === "light" ? "white" : "black",
+          minHeight: "100vh",
+        }}
+      >
+        <Head>
+          <title>Polkadot Dashboard UI</title>
+          <meta name="description" content="Polkadot Dashboard UI" />
+        </Head>
+        <main className="p-4 flex flex-col gap-8 text-color-text-primary">
+          <h3>
+            Network:{" "}
+            <button onClick={() => setNetwork("polkadot")}>Polkadot</button> |{" "}
+            <button onClick={() => setNetwork("kusama")}>Kusama</button> |{" "}
+            <button onClick={() => setNetwork("westend")}>Westend</button>.{" "}
+            Theme: <button onClick={() => setTheme("light")}>Light</button> |{" "}
+            <button onClick={() => setTheme("dark")}>Dark</button>
+          </h3>
+          <h1 className="text-3xl">Components</h1>
+          <h2 className="text-xl">Button Primary</h2>
 
-        <div className="theme-polkadot theme-light">
-          <ButtonPrimary text="Button" iconLeft={faUser} marginRight />
-          <ButtonPrimary text="Button" iconLeft={faUser} marginRight />
-          <ButtonPrimary
-            text="Button"
-            iconLeft={faUser}
-            iconRight={faUser}
-            lg
-            marginRight
-          />
-          <ButtonPrimary text="Button" lg disabled />
-        </div>
+          <div>
+            <ButtonPrimary text="Button" iconLeft={faUser} marginRight />
+            <ButtonPrimary text="Button" iconLeft={faUser} marginRight />
+            <ButtonPrimary
+              text="Button"
+              iconLeft={faUser}
+              iconRight={faUser}
+              lg
+              marginRight
+            />
+            <ButtonPrimary text="Button" lg disabled />
+          </div>
 
-        <h2 className="text-xl">Button Secondary</h2>
-        <div className="theme-polkadot theme-light">
-          <ButtonSecondary text="Button" marginRight />
-          <ButtonSecondary text="Button" iconLeft={faUser} marginRight />
-          <ButtonSecondary text="Button" iconRight={faUser} marginRight />
-          <ButtonSecondary
-            text="Button"
-            iconLeft={faUser}
-            iconRight={faUser}
-            marginRight
-          />
-          <ButtonSecondary text="Button" disabled />
-        </div>
+          <h2 className="text-xl">Button Secondary</h2>
+          <div>
+            <ButtonSecondary text="Button" marginRight />
+            <ButtonSecondary text="Button" iconLeft={faUser} marginRight />
+            <ButtonSecondary text="Button" iconRight={faUser} marginRight />
+            <ButtonSecondary
+              text="Button"
+              iconLeft={faUser}
+              iconRight={faUser}
+              marginRight
+            />
+            <ButtonSecondary text="Button" disabled />
+          </div>
 
-        <h2 className="text-xl">Button Invert</h2>
-        <div className="theme-polkadot theme-light">
-          <ButtonInvert text="Button" marginRight />
-          <ButtonInvert text="Button" iconLeft={faUserReg} marginRight />
-          <ButtonInvert text="Button" iconRight={faUserReg} marginRight />
-          <ButtonInvert
-            text="Button"
-            iconLeft={faUserReg}
-            iconRight={faUserReg}
-            marginRight
-          />
-          <ButtonInvert text="Button" disabled />
-        </div>
+          <h2 className="text-xl">Button Invert</h2>
+          <div>
+            <ButtonInvert text="Button" marginRight />
+            <ButtonInvert text="Button" iconLeft={faUserReg} marginRight />
+            <ButtonInvert text="Button" iconRight={faUserReg} marginRight />
+            <ButtonInvert
+              text="Button"
+              iconLeft={faUserReg}
+              iconRight={faUserReg}
+              marginRight
+            />
+            <ButtonInvert text="Button" disabled />
+          </div>
 
-        <h2 className="text-xl">Button Invert Rounded</h2>
-        <div className="theme-polkadot theme-light">
-          <ButtonInvertRounded text="Button" marginRight />
-          <ButtonInvertRounded text="Button" iconLeft={faUserReg} marginRight />
-          <ButtonInvertRounded
-            text="Button"
-            iconRight={faUserReg}
-            marginRight
-          />
-          <ButtonInvertRounded
-            text="Button"
-            iconLeft={faUserReg}
-            iconRight={faUserReg}
-            marginRight
-          />
-          <ButtonInvertRounded text="Button" disabled />
-        </div>
+          <h2 className="text-xl">Button Invert Rounded</h2>
+          <div>
+            <ButtonInvertRounded text="Button" marginRight />
+            <ButtonInvertRounded
+              text="Button"
+              iconLeft={faUserReg}
+              marginRight
+            />
+            <ButtonInvertRounded
+              text="Button"
+              iconRight={faUserReg}
+              marginRight
+            />
+            <ButtonInvertRounded
+              text="Button"
+              iconLeft={faUserReg}
+              iconRight={faUserReg}
+              marginRight
+            />
+            <ButtonInvertRounded text="Button" disabled />
+          </div>
 
-        <h2 className="text-xl">Button Submit</h2>
-        <div className="theme-polkadot theme-light">
-          <ButtonSubmit text="Button" marginRight />
-          <ButtonSubmit
-            text="Button"
-            iconLeft={faArrowAltCircleUp}
-            marginRight
-          />
-          <ButtonSubmit
-            text="Button"
-            iconRight={faArrowAltCircleUp}
-            marginRight
-          />
-          <ButtonSubmit
-            text="Button"
-            iconLeft={faArrowAltCircleUp}
-            iconRight={faArrowAltCircleUp}
-            marginRight
-          />
-          <ButtonSubmit text="Button" disabled />
-        </div>
-      </main>
-    </div>
+          <h2 className="text-xl">Button Submit</h2>
+          <div>
+            <ButtonSubmit text="Button" marginRight />
+            <ButtonSubmit
+              text="Button"
+              iconLeft={faArrowAltCircleUp}
+              marginRight
+            />
+            <ButtonSubmit
+              text="Button"
+              iconRight={faArrowAltCircleUp}
+              marginRight
+            />
+            <ButtonSubmit
+              text="Button"
+              iconLeft={faArrowAltCircleUp}
+              iconRight={faArrowAltCircleUp}
+              marginRight
+            />
+            <ButtonSubmit text="Button" disabled />
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
