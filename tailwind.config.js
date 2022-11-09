7958d621d9acc3751048f0b71fe17fc27aea2f99
Ross Bulat <ross@jkrbinvestments.com>
2022-11-09 20:08:14 +0000

    + westend-primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
           'polkadot-transparent': 'rgb(211, 48, 121, 0.05)',
           'kusama-transparent-light': 'rgb(51,51,51,0.05)',
           'kusama-transparent-dark': 'rgb(102,102,102, 0.05)',
+          'westend-primary': '#da4e71',
           'westend-transparent': 'rgb(218, 78, 113, 0.05)',
         },
         // text colors
