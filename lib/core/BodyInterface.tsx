2223a3468825f4ffbffdd44a76e16364ca68cb6f
Ross Bulat <ross@jkrbinvestments.com>
2022-11-09 18:56:25 +0000

    rm unneeded class

--- lib/core/BodyInterface.tsx
+++ lib/core/BodyInterface.tsx
@@ -7,7 +7,7 @@ export type BodyInterfaceProps = StyledComponentInterface;
  * An element that wraps the entire window, houses `SideInterface` and `BodyInterface`.
  */
 export const BodyInterface = ({ style, children }: BodyInterfaceProps) => (
-  <div className="box-border flex flex-auto flex-row relative" style={style}>
+  <div className="flex flex-auto flex-row relative" style={style}>
     {children}
   </div>
 );
