b44ddd9b947e150346a181c1cba33a5ef460ba78
TingALin <linshaoting6@gmail.com>
2023-02-22 21:53:55 +0800

    add more props

--- lib/buttons/ButtonHelp.tsx
+++ lib/buttons/ButtonHelp.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { InfoSVG } from "../svg/Info";
 import { valEmpty } from "../utils";
+import { CommonButtonInterface } from "../types";
 
-export type ButtonHelpProps = {
-  onClick?: () => void;
+export type ButtonHelpProps = CommonButtonInterface & {
   light?: boolean;
+  style?: React.CSSProperties;
 };
 
 /*
  * Help button used throughout dashboard apps.
  */
-export const ButtonHelp = ({ onClick, light }: ButtonHelpProps) => (
+export const ButtonHelp = ({
+  onClick,
+  light,
+  style,
+  disabled,
+  marginLeft,
+  marginRight,
+  marginX,
+  grow,
+}: ButtonHelpProps) => (
   <button
-    className={"btn-help" + valEmpty(light, "light")}
+    className={
+      "btn-help" +
+      valEmpty(light, "light") +
+      valEmpty(grow, "grow") +
+      valEmpty(marginRight, "m-right") +
+      valEmpty(marginLeft, "m-left") +
+      valEmpty(marginX, "m-x")
+    }
+    style={style}
+    disabled={disabled}
     onClick={() => {
       if (typeof onClick == "function") {
         onClick();
