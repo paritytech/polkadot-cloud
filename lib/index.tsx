02ba9527bc2c2e3f45029770d13c3337f7cdbe33
Ross Bulat <ross@parity.io>
2023-03-12 13:28:29 +0700

    move package to polkadotcloud/dashboard-ui

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,5 @@
 import "../styles/index.scss";
 
-// Core
-export { BodyInterface } from "./core/BodyInterface";
-
 // Buttons
 export { ButtonPrimary } from "./buttons/ButtonPrimary";
 export { ButtonSecondary } from "./buttons/ButtonSecondary";
