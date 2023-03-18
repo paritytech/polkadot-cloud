0622ad843519f1d661f3a09bdcf1f4a98b53fe01
Ross Bulat <ross@parity.io>
2023-03-18 15:46:44 +0700

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+// Copyright 2023 @paritytech/polkadot-dashboard-ui authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
