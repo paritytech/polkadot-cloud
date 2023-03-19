e945f91b2a15c47e3845602881869a2caa9300ce
Ross Bulat <ross@parity.io>
2023-03-19 10:48:20 +0700

    server.port

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./sandbox",
+  server: {
+    port: 5174,
+  },
   plugins: [react()],
 });
