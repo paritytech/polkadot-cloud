82343af69db7ec59d164fc114bc0e592a8e47735
Ross Bulat <ross@parity.io>
2023-03-28 10:42:06 +0700

    add comments

--- lib/core/index.tsx
+++ lib/core/index.tsx
@@ -11,12 +11,18 @@ export type EntryProps = ComponentBase & {
   network: Networks;
 };
 
+/* Entry
+ * The outer-most wrapper that hosts core tag styling.
+ */
 export const Entry = ({ children, style, mode, network }: EntryProps) => (
   <div className={`core-entry theme-${mode} theme-${network}`} style={style}>
     {children}
   </div>
 );
 
+/* Body
+ * An element that houses Side and Main.
+ */
 export const Body = ({ children, style }: ComponentBase) => {
   return (
     <div className="body-interface" style={style}>
@@ -25,6 +31,9 @@ export const Body = ({ children, style }: ComponentBase) => {
   );
 };
 
+/* Main
+ * A column flex wrapper that hosts the main page content.
+ */
 export const Main = forwardRef(
   ({ children, style }: ComponentBase, ref?: RefObject<HTMLDivElement>) => (
     <div ref={ref} className="main-interface" style={style}>
