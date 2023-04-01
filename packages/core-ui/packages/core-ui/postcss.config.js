270517ae64c48b1b80af929de4c44c384443fdd0
Ross Bulat <ross@parity.io>
2023-04-01 22:52:06 +0700

    init mono repo

--- packages/core-ui/packages/core-ui/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 @paritytech/polkadot-dashboard-ui authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
-};
