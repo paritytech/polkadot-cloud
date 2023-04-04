f40967fb2ec0ef3412f7ec2158ff17e0b8202359
Ross Bulat <ross@parity.io>
2023-04-04 18:14:05 +0700

    add HideScrollable & PageRow

--- packages/core-ui/lib/core/types.ts
+++ packages/core-ui/lib/core/types.ts
@@ -16,3 +16,8 @@ export type SideProps = ComponentBase & {
   // whether side menu is in minimised state
   minimised: boolean;
 };
+
+export type PageRowProps = ComponentBase & {
+  // whether there is margin space vertically
+  noVerticalMargin?: boolean;
+};
