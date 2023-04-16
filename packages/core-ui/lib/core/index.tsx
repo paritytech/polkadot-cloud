df4cdef4fe91de9d05b069888f543ea106780654
TingALin <linshaoting6@gmail.com>
2023-04-16 11:50:04 +0800

    reformat comments

--- packages/core-ui/lib/core/index.tsx
+++ packages/core-ui/lib/core/index.tsx
@@ -16,9 +16,9 @@ import { ButtonSecondary } from "../buttons/ButtonSecondary";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { ButtonTab } from "../buttons/ButtonTab";
 
-/* Entry
- *
- * The outer-most wrapper that hosts core tag styling.
+/**
+ * @name Entry
+ * @description The outer-most wrapper that hosts core tag styling.
  */
 export const Entry = ({ children, style, mode, network }: EntryProps) => (
   <div className={`core-entry theme-${mode} theme-${network}`} style={style}>
@@ -26,9 +26,9 @@ export const Entry = ({ children, style, mode, network }: EntryProps) => (
   </div>
 );
 
-/* Body
- *
- * An element that houses Side and Main.
+/**
+ * @name Body
+ * @description An element that houses Side and Main.
  */
 export const Body = ({ children, style }: ComponentBase) => {
   return (
@@ -38,9 +38,9 @@ export const Body = ({ children, style }: ComponentBase) => {
   );
 };
 
-/* Main
- *
- * A column flex wrapper that hosts the main page content.
+/**
+ * @name Main
+ * @description A column flex wrapper that hosts the main page content.
  */
 export const Main = forwardRef(
   ({ children, style }: ComponentBase, ref?: RefObject<HTMLDivElement>) => (
@@ -51,10 +51,11 @@ export const Main = forwardRef(
 );
 Main.displayName = "Main";
 
-/* Page
- *
- * A motion.div that wraps every page. Transitions can be applied to this wrapper that will affect
- * the entire page.
+/**
+ * @name Page
+ * @description
+ * A motion.div that wraps every page.
+ * Transitions can be applied to this wrapper that will affect the entire page.
  */
 export const Page = ({ children, style }: ComponentBase) => {
   return (
@@ -71,10 +72,10 @@ export const Page = ({ children, style }: ComponentBase) => {
   );
 };
 
-/* Side
- *
- * An element that houses the side menu and transitions to a toggle-able fixed overlay on smaller
- * screens. Handles maximised and minimised transitions.
+/**
+ * @name Side
+ * @description An element that houses the side menu and transitions to a toggle-able fixed overlay on smaller screens.
+ * @summary Handles maximised and minimised transitions.
  */
 export const Side = ({ children, style, open, minimised }: SideProps) => (
   <div
@@ -88,9 +89,11 @@ export const Side = ({ children, style, open, minimised }: SideProps) => (
   </div>
 );
 
-/* PageTitle
- *
- * The element that wraps a page title. Determines the padding and position relative to top of screen when the element is stuck.
+/**
+ * @name PageTitle
+ * @description
+ * The element that wraps a page title.
+ * Determines the padding and position relative to top of screen when the element is stuck.
  */
 export const PageTitle = ({ title, button, tabs = [] }: PageTitleProps) => {
   const [sticky, setSticky] = useState(false);
@@ -147,9 +150,9 @@ export const PageTitle = ({ title, button, tabs = [] }: PageTitleProps) => {
 
 PageTitle.displayName = "PageTitle";
 
-/* PageTitleTabs
- *
- * The element in a page title. Inculding the ButtonTab.
+/**
+ * @name PageTitleTabs
+ * @description The element in a page title. Inculding the ButtonTab.
  */
 export const PageTitleTabs = ({ sticky, tabs = [] }: PageTitleProps) => (
   <section className={`core-page-title-tabs${valEmpty(sticky, "sticky")}`}>
@@ -170,8 +173,9 @@ export const PageTitleTabs = ({ sticky, tabs = [] }: PageTitleProps) => (
   </section>
 );
 
-/* HideScrollable
- *
+/**
+ * @name HideScrollable
+ * @description
  * A fixed block that is used to hide scrollable content on smaller screens when a PageTitle is fixed.
  * Purely cosmetic. Applied in Pagetitle.
  */
@@ -181,9 +185,9 @@ export const HideScrollable = ({ children, style }: ComponentBase) => (
   </div>
 );
 
-/* PageRow
- *
- * Used to separate page content based on rows. Commonly used with RowPrimary and RowSecondary.
+/**
+ * @name PageRow
+ * @description Used to separate page content based on rows. Commonly used with RowPrimary and RowSecondary.
  */
 export const PageRow = ({ children, style, yMargin }: RowProps) => (
   <div
@@ -194,9 +198,9 @@ export const PageRow = ({ children, style, yMargin }: RowProps) => (
   </div>
 );
 
-/* Separator
- *
- * A horizontal spacer with a bottom border. General spacer for separating content by row.
+/**
+ * @name Separator
+ * @description A horizontal spacer with a bottom border. General spacer for separating content by row.
  */
 export const Separator = ({ children, style }: ComponentBase) => (
   <div className="core-separator" style={style}>
@@ -204,9 +208,9 @@ export const Separator = ({ children, style }: ComponentBase) => (
   </div>
 );
 
-/* PageHeading
- *
- * Positioned under titles for a Go Back button and other page header info.
+/**
+ * @name PageHeading
+ * @description Positioned under titles for a Go Back button and other page header info.
  */
 export const PageHeading = ({ children, style }: ComponentBase) => (
   <div className="core-page-heading" style={style}>
@@ -214,9 +218,9 @@ export const PageHeading = ({ children, style }: ComponentBase) => (
   </div>
 );
 
-/* ButtonRow
- *
- * A flex container for a row of buttons
+/**
+ * @name ButtonRow
+ * @description A flex container for a row of buttons
  */
 export const ButtonRow = ({ children, style, yMargin }: RowProps) => (
   <div
