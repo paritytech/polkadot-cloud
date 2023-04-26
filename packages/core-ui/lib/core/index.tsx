0547ea2421890b7f97d7a8a2f3cfbec763fdb482
TingALin <linshaoting6@gmail.com>
2023-04-26 10:46:22 +0800

    trim

--- packages/core-ui/lib/core/index.tsx
+++ packages/core-ui/lib/core/index.tsx
@@ -105,7 +105,7 @@ export const PageTitle = ({ title, button, tabs = [] }: PageTitleProps) => {
     if (cachedRef) {
       observer.observe(cachedRef);
     }
-    // unmount
+    // unmount.
     return () => {
       if (cachedRef) {
         observer.unobserve(cachedRef);
