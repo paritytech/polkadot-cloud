6aab052dfbdf05ccbceeeb2932d7664bae0ee3d5
TingALin <linshaoting6@gmail.com>
2023-04-27 13:25:58 +0800

    fixes

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.scss";
-import "../../packages/odometer/mini.css";
+import "../../packages/odometers/mini.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
