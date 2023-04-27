dc3e5b5a5d1b5abbb478631f9274ae401a032f8b
TingALin <linshaoting6@gmail.com>
2023-04-27 18:49:16 +0800

    rename mini.css

--- packages/odometers/lib/index.tsx
+++ packages/odometers/lib/index.tsx
@@ -4,7 +4,7 @@ SPDX-License-Identifier: Apache-2.0 */
 import { useEffect, useRef, type FC } from "react";
 import { ReactOdometerProps } from "./types";
 import Odometer from "odometer";
-import "../mini.css";
+import "../styles/index.css";
 
 export const ReactOdometer: FC<ReactOdometerProps> = ({ duration, value }) => {
   const node = useRef<HTMLDivElement>(null);
