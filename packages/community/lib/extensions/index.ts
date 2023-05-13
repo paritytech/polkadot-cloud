2a7bbd78fd7425152c37cdfb0b0146bb0875ab81
TingALin <linshaoting6@gmail.com>
2023-05-13 16:12:46 +0800

    /* eslint-disable */

--- packages/community/lib/extensions/index.ts
+++ packages/community/lib/extensions/index.ts
@@ -1,5 +1,6 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+/* eslint-disable */
 
 import type { ExtensionConfig } from "./types";
 const EnkryptSVG = require("./icons/enkrypt.svg");
