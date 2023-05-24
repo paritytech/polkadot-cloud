59999da0a8bebd128f03591d1d0ee0db5696ca13
TingALin <linshaoting6@gmail.com>
2023-05-24 08:07:19 +0800

    initial

---
+++ packages/core-ui/lib/buttons/ButtonOption.tsx
@@ -0,0 +1,12 @@
+/* @license Copyright 2023 @paritytech/polkadot-dashboard-ui authors & contributors
+SPDX-License-Identifier: Apache-2.0 */
+
+import { ButtonCommonProps, ComponentBase } from "../types";
+
+export type ButtonOptionProps = ComponentBase & ButtonCommonProps;
+
+/**
+ * @name ButtonOption
+ * @description Option button for different action.
+ */
+export const ButtonOption = ({}: ButtonOptionProps) => <button></button>;
