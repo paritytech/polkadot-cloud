347f1837bf8997714a30474f9bcc543f17d4e285
Ross Bulat <ross@parity.io>
2023-07-17 17:56:19 +0700

    mv fonts back

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.scss";
-import "../../packages/core-ui/fonts/index.css";
+import "../../packages/core-ui/lib/styles/fonts/index.css";
 import "../../packages/react-odometer/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
