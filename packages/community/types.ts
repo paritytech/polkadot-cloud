420dc7f96f5ebb530e8f44b1b4757cbd1ad22cc5
Ross Bulat <ross@parity.io>
2023-07-28 13:21:02 +0700

    add TODOs, add misc comment

--- packages/community/types.ts
+++ packages/community/types.ts
@@ -1,18 +1,12 @@
 /* @license Copyright 2023 @paritytech/polkadot-dashboard-ui authors & contributors
 SPDX-License-Identifier: Apache-2.0 */
 
-declare global {
-  interface Window {
-    // Nova Wallet will have this window property.
-    // eslint-disable-next-line
-    walletExtension?: any;
-  }
-}
-
 // The supported chains.
+// TODO: provide instructions on how to add your own chain.
 export type SupportedChains = "polkadot" | "kusama" | "westend";
 
 // Structure for an extension configuration.
+// TODO: provide url to add your own extension config.
 export interface ExtensionConfig {
   id: string;
   title: string;
@@ -21,6 +15,7 @@ export interface ExtensionConfig {
 }
 
 // Structure for a validator entity.
+// TODO: provide url to add your own validator config.
 export interface ValidatorConfig {
   name: string;
   thumbnail: string;
@@ -33,4 +28,13 @@ export interface ValidatorConfig {
   validators: Partial<{
     [K in SupportedChains]: string[];
   }>;
+}
+
+// Miscellaneous types.
+declare global {
+  interface Window {
+    // Nova Wallet will have this window property.
+    // eslint-disable-next-line
+    walletExtension?: any;
+  }
 }
