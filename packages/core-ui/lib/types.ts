31944ba6a66eb203d5b237694bd89b940f4a797c
Ross Bulat <ross@parity.io>
2023-08-08 20:56:11 +0700

    tidy up

--- packages/core-ui/lib/types.ts
+++ packages/core-ui/lib/types.ts
@@ -1,12 +1,6 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: Apache-2.0 */
 
-// A list of supported theme modes
-export type ThemeMode = "light" | "dark";
-
-// A list of supported networks
-export type Network = "polkadot" | "kusama" | "westend";
-
 // A generic type to handle React components. We assume the component may have
 // children and styling applied to it.
 export interface ComponentBase {
