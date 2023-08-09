ac5f3bfec505b08b0fc90b269c016b29d8f0b9b5
Ross Bulat <ross@parity.io>
2023-08-09 16:32:16 +0700

    fix sandbox

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.scss";
 
-import "../../packages/themes/theme/polkadot-relay/index.css";
-import "../../packages/themes/theme/kusama-relay/index.css";
-import "../../packages/themes/theme/westend-relay/index.css";
+import "../../packages/themes/lib/theme/polkadot-relay/index.css";
+import "../../packages/themes/lib/theme/kusama-relay/index.css";
+import "../../packages/themes/lib/theme/westend-relay/index.css";
 
 import "../../packages/core-ui/lib/styles/index.scss";
 
-import "../../packages/themes/template/default/fonts/index.css";
-import "../../packages/react-odometer/styles/index.scss";
+import "../../packages/themes/lib/template/default/fonts/index.css";
+import "../../packages/react-odometer/lib/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
