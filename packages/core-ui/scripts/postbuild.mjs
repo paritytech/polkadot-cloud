606046a0bca068083437c7e78334e5e368afc129
Ross Bulat <ross@parity.io>
2023-08-09 17:41:28 +0700

    add postbuild

---
+++ packages/core-ui/scripts/postbuild.mjs
@@ -0,0 +1,29 @@
+import fs from "fs";
+import { exec } from "child_process";
+
+const main = async () => {
+  try {
+    // Generate package.json and inject.
+    exec(
+      "node ../../.scripts/generatePackageJson.cjs -p core-ui -m index.tsx",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      }
+    );
+
+    // Rmmove generated content.
+    fs.unlinkSync(`./lib/index.tsx`);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+await main();
