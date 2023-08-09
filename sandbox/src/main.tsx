b263d0753ccc5befa17eb684341225c3a9081755
Nikos Kontakis <wirednkod@gmail.com>
2023-08-09 13:10:21 +0300

    Minor fix on grid row width; bump version

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.scss";
 
+<<<<<<< Updated upstream
 import "../../packages/themes/lib/theme/polkadot-relay/index.css";
 import "../../packages/themes/lib/theme/kusama-relay/index.css";
 import "../../packages/themes/lib/theme/westend-relay/index.css";
+=======
+import "../../packages/themes/theme/polkadot-relay/index.css";
+import "../../packages/themes/theme/kusama-relay/index.css";
+import "../../packages/themes/theme/westend-relay/index.css";
+import "../../packages/themes/theme/xcm/index.css";
+>>>>>>> Stashed changes
 
 import "../../packages/core-ui/lib/styles/index.scss";
 
