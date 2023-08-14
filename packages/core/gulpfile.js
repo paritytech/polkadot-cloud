421115d710614ddcd846f20ed6d6fb1edee11ff7
Nikos Kontakis <wirednkod@gmail.com>
2023-08-14 18:24:44 +0300

    Fix license issue

--- packages/core/gulpfile.js
+++ packages/core/gulpfile.js
@@ -1,4 +1,7 @@
-const { src, dest, watch, series } = require("gulp");
+/* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
+const { src, dest, series, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildTemplate() {
@@ -17,6 +20,12 @@ function buildComponents() {
   return src("scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(dest("dist/css"));
+}
+
+function watchTask() {
+  watch(["scss/**/*.scss"], buildComponents);
+  watch(["theme/**/*.css"], buildTheme);
+  watch(["template/**/*.css"], buildTemplate);
 }
 
 exports.default = series(buildTemplate, buildTheme, buildComponents);
