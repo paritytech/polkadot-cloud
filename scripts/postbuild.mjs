35bc1d3ff758711197870c9d0aec7e76ea5ff852
TingALin <linshaoting6@gmail.com>
2023-08-15 11:42:09 +0700

    Post-build checks

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -4,6 +4,30 @@
 import fs from "fs";
 import { dirFilesExist } from "./index.mjs";
 
+const matchName = async (dir, files) => {
+  for (let file of files) {
+    fs.stat(`${dir}${file}/dist/package.json`, (err) => {
+      if (err) {
+        console.error(`❌ dist/package.json directory not found`);
+        return;
+      }
+      const json = JSON.parse(
+        fs.readFileSync(`${dir}${file}/dist/package.json`).toString()
+      );
+
+      const name = Object.entries(json).filter((p) => {
+        return "name".includes(p[0]);
+      });
+
+      if (name[0][1] != `@polkadotcloud/${json.name.split(/\/(.*)/s)[1]}`) {
+        console.error(
+          `❌ ${name[0][1]} dist/package name doesn't meet the naming requirement`
+        );
+      }
+    });
+  }
+};
+
 try {
   // Ensure that the package directory exists.
   fs.readdir("./packages", async (err, files) => {
@@ -14,6 +38,8 @@ try {
 
     // Check `dist` exist in each package.
     await dirFilesExist("./packages/", files, ["dist"]);
+
+    await matchName("./packages/", files);
 
     console.log(`✅ Post-build integrity checks complete.`);
   });
