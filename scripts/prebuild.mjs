8cf72a0c79aa759e29aeee778fa830714b5f2d44
TingALin <linshaoting6@gmail.com>
2023-08-16 08:36:13 +0700

    includes -> indexOf

--- scripts/prebuild.mjs
+++ scripts/prebuild.mjs
@@ -18,7 +18,7 @@ const dirFoldersOnly = async (dir, files) => {
   }
 };
 
-const matchNameNScripts = (dir, files) => {
+const matchScripts = (dir, files) => {
   for (let file of files) {
     fs.stat(`${dir}${file}/package.json`, (err) => {
       if (err) {
@@ -36,7 +36,9 @@ const matchNameNScripts = (dir, files) => {
       }
 
       if (
-        !Object.keys(json?.scripts).includes("build:mock" && "build" && "clear")
+        Object.keys(json?.scripts).indexOf("build:mock") === "-1" ||
+        Object.keys(json?.scripts).indexOf("build") === "-1" ||
+        Object.keys(json?.scripts).indexOf("clear") === "-1"
       ) {
         console.error(
           `❌ All of the scripts field in package.json are required to have build:mock, build and clear properties`
@@ -63,7 +65,7 @@ try {
       "package.json",
       "lib",
     ]);
-    matchNameNScripts("./packages/", files);
+    matchScripts("./packages/", files);
 
     console.log(`✅ Pre-build integrity checks complete.`);
   });
