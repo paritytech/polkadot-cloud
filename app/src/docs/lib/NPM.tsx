0ac8d50876829f7f3a0f5c08aeab5b8841955ef6
Ross Bulat <ross@parity.io>
2023-08-18 16:45:20 +0700

    npm header

---
+++ app/src/docs/lib/NPM.tsx
@@ -0,0 +1,24 @@
+/* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
+import { faNpm } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export interface Props {
+  npmPackage: string;
+}
+
+export const NPM = ({ npmPackage }: Props) => {
+  return (
+    <h3>
+      <FontAwesomeIcon icon={faNpm} />
+      <a
+        href={`https://www.npmjs.com/npmPackage/${npmPackage}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {npmPackage}
+      </a>
+    </h3>
+  );
+};
