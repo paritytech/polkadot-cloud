cd365f0acc698e3d817dcbb91616e783310ee33d
Ross Bulat <ross@parity.io>
2023-08-23 16:43:33 +0700

    remove sass

--- packages/cloud-react/gulpfile.js
+++ packages/cloud-react/gulpfile.js
@@ -5,22 +5,10 @@ SPDX-License-Identifier: GPL-3.0-only */
 import gulp from "gulp";
 import lrserver from "tiny-lr";
 import refresh from "gulp-livereload";
-import * as dartSass from "sass";
-import gulpSass from "gulp-sass";
 import ts from "gulp-typescript";
 import strip from "gulp-strip-comments";
 
-const sass = gulpSass(dartSass);
-
 const { src, dest, series } = gulp;
-const SASS_OPTIONS = { outputStyle: "compressed" };
-
-const buildScss = () => {
-  return src("lib/**/*.scss")
-    .pipe(sass(SASS_OPTIONS))
-    .pipe(dest("dist"))
-    .pipe(refresh(lrserver));
-};
 
 const buildComponents = () => {
   const tsProject = ts.createProject("tsconfig.json");
@@ -40,9 +28,4 @@ const licenseAndReadme = () => {
   return src(["LICENSE", "README.md"]).pipe(dest("dist"));
 };
 
-export default series(
-  buildScss,
-  buildComponents,
-  stripComments,
-  licenseAndReadme
-);
+export default series(buildComponents, stripComments, licenseAndReadme);
