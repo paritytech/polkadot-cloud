4c1fc13cdc3e2f9436add27c881c537caf2fe4f1
Ross Bulat <ross@parity.io>
2023-08-23 12:51:10 +0700

    unimplemented tweaks

--- packages/utils/lib/index.ts
+++ packages/utils/lib/index.ts
@@ -431,7 +431,8 @@ export const makeCancelable = (promise: Promise<AnyObject>) => {
 /**
  * @name unimplemented
  * @summary A placeholder function to signal a deliberate unimplementation.
+ * Consumes an arbitrary number of props.
+ * @returns undefined.
  */
-export const unimplemented = () => {
-  /* unimplemented. */
-};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+export const unimplemented = ({ ...props }) => undefined;
