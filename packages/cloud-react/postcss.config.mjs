7cb2e7e0f0ced9dc9eeb0a2c081072081c372a16
Ross Bulat <ross@parity.io>
2023-08-24 16:55:49 +0700

    del unused file

--- packages/cloud-react/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
-SPDX-License-Identifier: GPL-3.0-only */
-
-export default {
-  plugins: {
-    autoprefixer: {},
-  },
-};
