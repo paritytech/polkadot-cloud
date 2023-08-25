b3619c478fc60fedb155fab0e11eee5417b3a117
Ross Bulat <ross@parity.io>
2023-08-25 19:55:33 +0700

    ci

--- packages/dev-tools/lib/index.ts
+++ packages/dev-tools/lib/index.ts
@@ -1,4 +1,4 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-console.log("Hello, world!");
+console.log("Hello, world! Coming soon");
