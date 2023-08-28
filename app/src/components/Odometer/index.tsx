2dd2e6ef09121cd2e1667a92784e1dd53e97581a
Ross Bulat <ross@parity.io>
2023-08-28 21:08:57 +0700

    get digit patterns working

--- app/src/components/Odometer/index.tsx
+++ app/src/components/Odometer/index.tsx
@@ -1,40 +1,147 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, createRef, MutableRefObject } from "react";
 
 interface Props {
   value: number | string;
 }
 
-export const Odometer = (props: Props) => {
-  // Store the current odometer value in state.
-  const [value, setValue] = useState<string>(String(props.value));
+type Status = "new" | "transition" | "stable";
 
-  // Store the digits of the current value
+export const Odometer = (props: Props) => {
+  // Store the digits of the current value.
   const [digits, setDigits] = useState<string[]>([]);
 
-  // When a new value is provided, update the odometer value.
-  useEffect(() => {
-    // TODO: calculate new digits
-    const newDigits = value.toString().split("");
+  // Store digits of the previous value.
+  const [prevDigits, setPrevDigits] = useState<string[]>([]);
 
+  // Store the status of the odometer (transitioning or stable).
+  const [status, setStatus] = useState<Status>("stable");
+
+  // Store whether component has iniiialized.
+  const [initialized, setInitialized] = useState<boolean>(false);
+
+  // Store refs of each digit
+  const [digitRefs, setDigitRefs] = useState<
+    MutableRefObject<HTMLSpanElement>[]
+  >([]);
+
+  // Phase 1: new digits and refs are added to the odometer.
+  useEffect(() => {
+    const newDigits = props.value.toString().split("");
     setDigits(newDigits);
 
-    // TODO: determine animations (up or down) (digits to animate).
+    if (!initialized) {
+      setInitialized(true);
+    } else {
+      setStatus("new");
+      setPrevDigits(digits);
+    }
+    setDigitRefs(Array(newDigits.length).fill(createRef()));
+  }, [props.value]);
 
-    setValue(value);
-  }, [value]);
+  // Phase 2: set up digit animation.
+  useEffect(() => {
+    if (status === "new" && !digitRefs.find((d) => d.current === null)) {
+      setStatus("transition");
+      setTimeout(() => setStatus("stable"), 10000);
+    }
+  }, [status, digitRefs]);
+
+  const allDigits = [
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "0",
+    ".",
+    undefined,
+  ];
 
   return (
-    <div className="odometer">
+    <span className="odometer">
       {digits.map((d, i) => {
+        // If transitioning, get digits needed to animate.
+        let digitHeight: string | number = "inherit";
+        let childDigits = null;
+        if (status === "transition") {
+          digitHeight = digitRefs[i]?.current?.offsetHeight;
+
+          let digitsToAnimate = [];
+          const digitIndex = allDigits.indexOf(digits[i]);
+          const prevDigitIndex = allDigits.indexOf(prevDigits[i]);
+          const difference = Math.abs(digitIndex - prevDigitIndex);
+          const direction =
+            digitIndex > prevDigitIndex
+              ? "up"
+              : digitIndex < prevDigitIndex
+              ? "down"
+              : "none";
+
+          if (direction !== "none") {
+            // If transitioning between a . or undefined, only animate the current digit.
+            if (
+              [".", undefined].includes(prevDigits[i]) ||
+              [".", undefined].includes(digits[i])
+            ) {
+              digitsToAnimate = [digits[i]];
+            } else {
+              // If transitioning between two digits, animate all digits in between.
+              if (digitIndex < prevDigitIndex) {
+                digitsToAnimate = Array.from(
+                  { length: difference },
+                  (_, k) => allDigits[prevDigitIndex - k - 1]
+                );
+              } else {
+                digitsToAnimate = Array.from(
+                  { length: difference },
+                  (_, k) => allDigits[k + prevDigitIndex + 1]
+                );
+              }
+            }
+          }
+
+          childDigits = digitsToAnimate.map((c, j) => {
+            const posKey = direction === "up" ? "bottom" : "top";
+            return (
+              <span
+                key={`child_digit_${j}`}
+                className="digit"
+                style={{
+                  position: "absolute",
+                  [posKey]: `${100 * (j + 1)}%`,
+                  left: 0,
+                  height: digitHeight,
+                }}
+              >
+                {c}
+              </span>
+            );
+          });
+
+          console.log(prevDigits[i], digits[i]);
+          console.log(digitsToAnimate);
+          console.log(direction);
+          console.log("---");
+        }
+
         return (
-          <span key={`digit_${i}`} className={`digit digit_${i}`}>
-            {d}
+          <span
+            key={`digit_${i}`}
+            ref={digitRefs[i]}
+            className={`digit digit_${i}`}
+          >
+            {status === "transition" && childDigits}
+            {status === "stable" ? d : prevDigits[i]}
           </span>
         );
       })}
-    </div>
+    </span>
   );
 };
--- app/src/docs/Odometer/index.tsx
+++ app/src/docs/Odometer/index.tsx
@@ -1,16 +1,33 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
+import { useState } from "react";
 import { Odometer as OdometerComponent } from "../../components/Odometer";
 
-export const Odometer = () => (
-  <div className="doc">
-    <h1>Odometer</h1>
+export const Odometer = () => {
+  const [val, setVal] = useState<number>(123.456);
 
-    <div style={{ margin: "2rem 0" }}>
-      <h1>
-        <OdometerComponent value={123.456} />
-      </h1>
+  const updateValue = () => setVal(val + 17491);
+
+  return (
+    <div className="doc">
+      <h1>Odometer</h1>
+
+      <div style={{ display: "flex" }}>
+        <button
+          type="button"
+          onClick={() => updateValue()}
+          style={{ marginTop: "5rem" }}
+        >
+          Update
+        </button>
+
+        <div style={{ margin: "2rem 0" }}>
+          <h1>
+            <OdometerComponent value={val} />
+          </h1>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
