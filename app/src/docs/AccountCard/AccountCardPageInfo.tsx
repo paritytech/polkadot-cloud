2500637caf1970590d0295de907151b7f4f96328
Nikos Kontakis <wirednkod@gmail.com>
2023-08-30 15:37:29 +0300

    minor fix on naming

--- app/src/docs/AccountCard/AccountCardPageInfo.tsx
+++ app/src/docs/AccountCard/AccountCardPageInfo.tsx
@@ -1,10 +1,6 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import {
-  AccountCard,
-  IconProps,
-} from "@packages/cloud-react/lib/recipes/AccountCard";
 import { SimpleEditor } from "../lib/SimpleEditor";
 
 export const AccountCardPageInfo = () => {
