2b81f8b5cbf2ad4c63e49e90e976a886b1477e59
Nikos Kontakis <wirednkod@gmail.com>
2023-09-01 15:21:10 +0300

    Add license

--- packages/utils/vite.config.js
+++ packages/utils/vite.config.js
@@ -1,3 +1,6 @@
+/* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
