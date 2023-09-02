383138a7b54c77f26ada1d997b7c7789de53dba7
Ross Bulat <ross@parity.io>
2023-09-02 13:37:23 +0700

    close n

--- app/src/docs/Utilities/PlanckToUnit.tsx
+++ app/src/docs/Utilities/PlanckToUnit.tsx
@@ -6,8 +6,7 @@ import { SimpleEditor } from "../lib/SimpleEditor";
 export const PlanckToUnit = () => {
   const code = `const inputValue = new BigNumber("10000000");
 const units = 6;
-fn.planckToUnit(inputValue, units); // 10.000000
-`;
+fn.planckToUnit(inputValue, units); // 10.000000`;
 
   return <SimpleEditor code={code} standalone />;
 };
--- app/src/docs/Utilities/RmCommas.tsx
+++ app/src/docs/Utilities/RmCommas.tsx
@@ -5,7 +5,7 @@ import { SimpleEditor } from "../lib/SimpleEditor";
 
 export const RmCommas = () => {
   const code = `const inputValue = "1,000,000";
-  const result = fn.rmCommas(inputValue); // 1000000`;
+const result = fn.rmCommas(inputValue); // 1000000`;
 
   return <SimpleEditor code={code} standalone />;
 };
