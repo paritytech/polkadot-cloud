c43942c9bff954652215a02f00ad3011a3b6498d
Ross Bulat <ross@parity.io>
2023-09-18 18:46:04 +0100

    style code

--- app/src/docs/Extensions/ValidatorOperator.tsx
+++ app/src/docs/Extensions/ValidatorOperator.tsx
@@ -5,6 +5,7 @@ import { SimpleEditor } from "../lib/SimpleEditor";
 
 export const ValidatorOperator = () => {
   const code = `// Import validator community object.
+
 import { ValidatorCommunity } from '@polkadot-cloud/community/extensions';`;
 
   return <SimpleEditor code={code} standalone />;
