9f6dc636a653022432729931b016189948b00372
Ross Bulat <ross@parity.io>
2023-09-18 18:22:52 +0100

    improve extensions docs

--- app/src/docs/Extensions/ExtensionsJsx.tsx
+++ app/src/docs/Extensions/ExtensionsJsx.tsx
@@ -2,32 +2,15 @@
 SPDX-License-Identifier: GPL-3.0-only */
 
 import { SimpleEditor } from "../lib/SimpleEditor";
-import { FearlessWallet } from "@packages/community/lib/extensions/jsx/FearlessWallet";
 import { Enkrypt } from "@packages/community/lib/extensions/jsx/Enkrypt";
-import { NovaWallet } from "@packages/community/lib/extensions/jsx/NovaWallet";
-import { PolkadotJS } from "@packages/community/lib/extensions/jsx/PolkadotJS";
-import { PolkaGate } from "@packages/community/lib/extensions/jsx/PolkaGate";
-import { SubwalletJS } from "@packages/community/lib/extensions/jsx/SubwalletJS";
-import { Talisman } from "@packages/community/lib/extensions/jsx/Talisman";
 import { Demo } from "../lib/Demo";
 
 export const ExtensionsJsx = () => {
-  const code = `import { FearlessWallet } from "@polkadot-cloud/community/extensions/jsx/FearlessWallet";
-import { NovaWallet } from "@packages/community/lib/extensions/jsx/NovaWallet";
-import { PolkadotJS } from "@polkadot-cloud/community/extensions/jsx/PolkadotJS";
-import { PolkaGate } from "@polkadot-cloud/community/extensions/jsx/PolkaGate";
-import { SubwalletJS } from "@polkadot-cloud/community/extensions/jsx/SubwalletJS";
-import { Talisman } from "@polkadot-cloud/community/extensions/jsx/Talisman";  
+  const code = `import { Enkrypt } from "@polkadot-cloud/community/extensions/jsx/Enkrypt";
 
 const App = () => (
   <>
     <Enkrypt />
-    <FearlessWallet />
-    <NovaWallet />
-    <PolkadotJs />
-    <PolkaGate />
-    <SubwalletJs />
-    <Talisman />
   </>
 )`;
 
@@ -36,24 +19,6 @@ const App = () => (
       <Demo showThemes={false}>
         <div className="svg-box">
           <Enkrypt />
-        </div>
-        <div className="svg-box">
-          <FearlessWallet />
-        </div>
-        <div className="svg-box">
-          <NovaWallet />
-        </div>
-        <div className="svg-box">
-          <PolkadotJS />
-        </div>
-        <div className="svg-box">
-          <PolkaGate />
-        </div>
-        <div className="svg-box">
-          <SubwalletJS />
-        </div>
-        <div className="svg-box">
-          <Talisman />
         </div>
       </Demo>
       <SimpleEditor code={code} language="javascript" />
