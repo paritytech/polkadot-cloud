aac8c746a13a8e295da2a9ad3775a35aa7c766cf
Nikos Kontakis <wirednkod@gmail.com>
2023-09-21 14:56:45 +0300

    reduce user data to 4 lines

--- app/src/docs/Validators/ValidatorOperator.tsx
+++ app/src/docs/Validators/ValidatorOperator.tsx
@@ -14,16 +14,11 @@ import { ValidatorCommunity } from '@polkadot-cloud/assets/validators';
 return (
   <>
   {ValidatorCommunity.map((v) => (
-    <Grid row key={v.name + "_i"}>
-      <Grid column key={v.name + "_i"}>
-        {v.name}
-      </Grid>
-      <Grid column key={v.email + "_i"}>
-        {v.email || "-'"}
-      </Grid>
-      <Grid column key={v.website + "_i"}>
-        {v.website || "-"}
-      </Grid>
+    <Grid row>
+      <Grid column>{v.name}</Grid>
+      <Grid column>{v.website}</Grid>
+      <Grid column>{v.twitter}</Grid>
+      <Grid column>{v.email}</Grid>
     </Grid>
   ))}
   </>
@@ -39,17 +34,32 @@ return (
         <Grid column key={"website_i"}>
           <h5>Website</h5>
         </Grid>
-      </Grid>
-      {ValidatorCommunity.map((v) => (
-        <Grid row key={v.name + "_i"}>
-          <Grid column key={v.name + "_i"}>
-            <p style={{ margin: 0 }}>{v.name}</p>
-          </Grid>
-          <Grid column key={v.website + "_i"}>
-            <p style={{ margin: 0 }}>{v.website || "-"}</p>
-          </Grid>
+        <Grid column key={"twitter_i"}>
+          <h5>Twitter</h5>
         </Grid>
-      ))}
+        <Grid column key={"twitter_i"}>
+          <h5>Email</h5>
+        </Grid>
+      </Grid>
+      {ValidatorCommunity.map((v, i) => {
+        if (i < 4)
+          return (
+            <Grid row key={v.name + "_i"}>
+              <Grid column key={v.name + "_i"}>
+                <p style={{ margin: 0 }}>{v.name}</p>
+              </Grid>
+              <Grid column key={v.website + "_i"}>
+                <p style={{ margin: 0 }}>{v.website || "-"}</p>
+              </Grid>
+              <Grid column key={v.twitter + "_i"}>
+                <p style={{ margin: 0 }}>{v.twitter || "-"}</p>
+              </Grid>
+              <Grid column key={v.email + "_i"}>
+                <p style={{ margin: 0 }}>{v.email || "-"}</p>
+              </Grid>
+            </Grid>
+          );
+      })}
       <div style={{ marginTop: "2.5rem" }}>
         <SimpleEditor code={code} standalone />
       </div>
