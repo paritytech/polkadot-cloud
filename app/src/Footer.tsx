78a6deb4456165afd01128e1b5a606422dcee4e3
Ross Bulat <ross@parity.io>
2023-09-21 13:55:25 +0100

    add app footer

---
+++ app/src/Footer.tsx
@@ -0,0 +1,46 @@
+// Copyright 2023 @paritytech/polkadot-cloud authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import IconSVG from "./svg/icon-filled.svg?react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <section>
+        <IconSVG className="icon" />
+        <div>
+          <h4>
+            &copy; Copyright &nbsp;
+            <span>Polkadot Cloud Authors & Contributors.</span>
+          </h4>
+        </div>{" "}
+        <div>
+          <h4 className="license">
+            Licensed with{" "}
+            <a
+              href="https://spdx.org/licenses/GPL-3.0-only.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GPL-3.0-only
+            </a>
+          </h4>
+        </div>
+      </section>
+      <section>
+        <h4>
+          <a
+            href="https://github.com/paritytech/polkadot-cloud"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            &nbsp; GitHub
+          </a>
+        </h4>
+      </section>
+    </div>
+  );
+};
--- app/src/Router.tsx
+++ app/src/Router.tsx
@@ -1,10 +1,11 @@
-// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
+// Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./config/routes";
 import { Error } from "./Error";
 import { Menu } from "./Menu";
+import { Footer } from "./Footer";
 
 export const Router = () => (
   <>
@@ -17,5 +18,6 @@ export const Router = () => (
         <Route key="nav_page_other" path="*" element={<Error />} />
       </Routes>
     </div>
+    <Footer />
   </>
 );
