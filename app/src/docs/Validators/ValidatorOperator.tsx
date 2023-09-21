833489c8867e66cbb982b2b3902f6b08e0c4d1ad
Nikos Kontakis <wirednkod@gmail.com>
2023-09-21 14:38:35 +0300

    Add Validator operators code usage example

--- app/src/docs/Validators/ValidatorOperator.tsx
+++ app/src/docs/Validators/ValidatorOperator.tsx
@@ -1,12 +1,58 @@
 /* @license Copyright 2023 @paritytech/polkadot-cloud authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
+import { Grid } from "@packages/cloud-react/lib/base/structure/Grid";
 import { SimpleEditor } from "../lib/SimpleEditor";
+import { ValidatorCommunity } from "@packages/assets/lib/validators";
 
 export const ValidatorOperator = () => {
   const code = `// Import validator community object.
 
-import { ValidatorCommunity } from '@polkadot-cloud/assets/validators';`;
+import { ValidatorCommunity } from '@polkadot-cloud/assets/validators';
+....
 
-  return <SimpleEditor code={code} standalone />;
+return (
+  <>
+  {ValidatorCommunity.map((v) => (
+    <Grid row key={v.name + "_i"}>
+      <Grid column key={v.name + "_i"}>
+        {v.name}
+      </Grid>
+      <Grid column key={v.email + "_i"}>
+        {v.email || "-'"}
+      </Grid>
+      <Grid column key={v.website + "_i"}>
+        {v.website || "-"}
+      </Grid>
+    </Grid>
+  ))}
+  </>
+)
+`;
+
+  return (
+    <>
+      <Grid row key={"1_i"}>
+        <Grid column key={"name_i"}>
+          <h5>Name</h5>
+        </Grid>
+        <Grid column key={"website_i"}>
+          <h5>Website</h5>
+        </Grid>
+      </Grid>
+      {ValidatorCommunity.map((v) => (
+        <Grid row key={v.name + "_i"}>
+          <Grid column key={v.name + "_i"}>
+            <p style={{ margin: 0 }}>{v.name}</p>
+          </Grid>
+          <Grid column key={v.website + "_i"}>
+            <p style={{ margin: 0 }}>{v.website || "-"}</p>
+          </Grid>
+        </Grid>
+      ))}
+      <div style={{ marginTop: "2.5rem" }}>
+        <SimpleEditor code={code} standalone />
+      </div>
+    </>
+  );
 };
