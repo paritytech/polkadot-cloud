304a3a1eaa0bddd00f3108156be1bf097e3fa6c7
Ross Bulat <ross@parity.io>
2023-09-29 16:11:52 +0700

    add ExtensionsProvider

---
+++ packages/cloud-react/lib/connect/ExtensionsProvider/defaults.ts
@@ -0,0 +1,15 @@
+// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function,  no-unused-vars */
+
+import type { ExtensionsContextInterface } from "./types";
+
+export const defaultExtensionsContext: ExtensionsContextInterface = {
+  extensions: [],
+  extensionsStatus: {},
+  extensionsFetched: false,
+  checkingInjectedWeb3: false,
+  setExtensionStatus: (id, status) => {},
+  setExtensionsFetched: (fetched) => {},
+  setExtensions: (extensions) => {},
+};
---
+++ packages/cloud-react/lib/connect/ExtensionsProvider/index.tsx
@@ -0,0 +1,128 @@
+// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { setStateWithRef } from "@polkadot-cloud/utils";
+import { ExtensionsArray } from "@polkadot-cloud/assets/extensions";
+import { ReactNode, useEffect, useRef, useState, createContext } from "react";
+import type {
+  ExtensionInjected,
+  ExtensionsContextInterface,
+  ExtensionsStatus,
+} from "./types";
+import { defaultExtensionsContext } from "./defaults";
+import { AnyJson } from "../../types";
+
+export const ExtensionsProvider = ({ children }: { children: ReactNode }) => {
+  // Store whether initial `injectedWeb3` checking is underway.
+  //
+  // Injecting `injectedWeb3` is an asynchronous process, so we need to check for its existence for
+  // a period of time.
+  const [checkingInjectedWeb3, setCheckingInjectedWeb3] =
+    useState<boolean>(true);
+  const checkingInjectedWeb3Ref = useRef(checkingInjectedWeb3);
+
+  // Store whether injected interval has been initialised.
+  const intervalInitialisedRef = useRef<boolean>(false);
+
+  // Store the installed extensions in state.
+  const [extensions, setExtensionsState] = useState<ExtensionInjected[] | null>(
+    null
+  );
+  const extensionsRef = useRef(extensions);
+
+  // Store whether extensions have been fetched.
+  const [extensionsFetched, setExtensionsFetched] = useState(false);
+
+  // Store each extension's status in state.
+  const [extensionsStatus, setExtensionsStatus] = useState<ExtensionsStatus>(
+    {}
+  );
+  const extensionsStatusRef = useRef(extensionsStatus);
+
+  // Setter for injected extensions.
+  const setExtensions = (e: ExtensionInjected[] | null) =>
+    setStateWithRef(e, setExtensionsState, extensionsRef);
+
+  // Listen for window.injectedWeb3 with an interval.
+  let injectedWeb3Interval: ReturnType<typeof setInterval>;
+  let injectCounter = 0;
+
+  // Handle completed interval check for `injectedWeb3`.
+  //
+  // If `injectedWeb3` is present, get installed extensions and add to state.
+  const handleClearInterval = (hasInjectedWeb3: boolean) => {
+    clearInterval(injectedWeb3Interval);
+    if (hasInjectedWeb3) {
+      setExtensions(getInstalledExtensions());
+    }
+    setStateWithRef(false, setCheckingInjectedWeb3, checkingInjectedWeb3Ref);
+  };
+
+  // Sets an interval to listen to `window` until the `injectedWeb3` property is present. Cancels
+  // after 500 * 10 milliseconds.
+  const checkEveryMs = 500;
+  const totalChecks = 10;
+  useEffect(() => {
+    if (!intervalInitialisedRef.current) {
+      intervalInitialisedRef.current = true;
+
+      injectedWeb3Interval = setInterval(() => {
+        if (++injectCounter === totalChecks) handleClearInterval(false);
+        else {
+          // if injected is present
+          const injectedWeb3 = (window as AnyJson)?.injectedWeb3 || null;
+          if (injectedWeb3 !== null) handleClearInterval(true);
+        }
+      }, checkEveryMs);
+    }
+    return () => clearInterval(injectedWeb3Interval);
+  });
+
+  // Setter for an extension status.
+  const setExtensionStatus = (id: string, status: string) => {
+    setStateWithRef(
+      Object.assign(extensionsStatusRef.current || {}, {
+        [id]: status,
+      }),
+      setExtensionsStatus,
+      extensionsStatusRef
+    );
+  };
+
+  // Getter for the currently installed extensions.
+  //
+  // Loops through the supported extensios and checks if they are present in `injectedWeb3`.
+  const getInstalledExtensions = () => {
+    const { injectedWeb3 }: AnyJson = window;
+    const installed: ExtensionInjected[] = [];
+    ExtensionsArray.forEach((e) => {
+      if (injectedWeb3[e.id] !== undefined) {
+        installed.push({
+          ...e,
+          ...injectedWeb3[e.id],
+        });
+      }
+    });
+    return installed;
+  };
+
+  return (
+    <ExtensionsContext.Provider
+      value={{
+        extensions: extensionsRef.current || [],
+        extensionsStatus: extensionsStatusRef.current,
+        checkingInjectedWeb3: checkingInjectedWeb3Ref.current,
+        extensionsFetched,
+        setExtensionStatus,
+        setExtensionsFetched,
+        setExtensions,
+      }}
+    >
+      {children}
+    </ExtensionsContext.Provider>
+  );
+};
+
+export const ExtensionsContext = createContext<ExtensionsContextInterface>(
+  defaultExtensionsContext
+);
