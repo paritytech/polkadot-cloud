200d3872193bf1deeb2177684780170c4ac33bb0
Ross Bulat <ross@parity.io>
2023-10-03 21:36:05 +0700

    fix code example

--- app/src/docs/GettingStarted/Installation/CSSThemes.tsx
+++ app/src/docs/GettingStarted/Installation/CSSThemes.tsx
@@ -6,7 +6,7 @@ import { SimpleEditor } from "@docs/SimpleEditor";
 export const CSSThemes = () => {
   const code = `// Top of app hierarchy. E.g. index.tsx, main.tsx.
 
-import "@polkadot-cloud/core/brand/polkadot-relay/index.css";
+import "@polkadot-cloud/core/accent/polkadot-relay.css";
 import "@polkadot-cloud/core/theme/cloud/index.css";
 
 ...
