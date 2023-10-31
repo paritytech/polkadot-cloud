db874c6a7e32f2723b4b4c71d978f1c3f137e7a3
wirednkod <wirednkod@gmail.com>
2023-10-31 16:58:31 +0200

    Add ConnectProvider

---
+++ packages/cloud-react/lib/recipes/Connect/Providers/ConnectProvider.tsx
@@ -0,0 +1,38 @@
+import { VaultHardwareProvider } from "./HardwareProviders/Vault";
+import { LedgerHardwareProvider } from "./HardwareProviders/Ledger";
+import { NotificationsProvider } from "./NotificationsProvider";
+import { ExtensionsProvider } from "../../../connect/ExtensionsProvider";
+import { ExtensionAccountsProvider } from "../../../connect/ExtensionAccountsProvider";
+import { OtherAccountsProvider } from "./OtherAccountsProvider";
+import { ImportedAccountsProvider } from "./ImportedAccountsProvider";
+import { HelpProvider } from "./HelpProvider";
+import { PromptProvider } from "./PromptProvider";
+import { ConnectType, DappInfo } from "./ConnectConfigProvider/types";
+import { OverlayProvider } from "../../../overlay/OverlayProvider";
+
+const provider = (prov, props = {}) => [prov, props];
+
+export const ConnectProvider = ({ providers, children }) => {
+  for (let i = providers.length - 1; i >= 0; --i) {
+    const [Provider, props] = providers[i];
+    children = <Provider {...props}>{children}</Provider>;
+  }
+  return children;
+};
+
+export const connectInfo = (appInfo: DappInfo, connInfo: ConnectType) => {
+  const providers = [];
+  providers.push(provider(NotificationsProvider));
+  if (connInfo.hardwareActive) {
+    providers.push(provider(LedgerHardwareProvider));
+    providers.push(provider(VaultHardwareProvider));
+  }
+  providers.push(provider(ExtensionsProvider));
+  providers.push(provider(ExtensionAccountsProvider, appInfo));
+  providers.push(provider(OtherAccountsProvider));
+  providers.push(provider(ImportedAccountsProvider));
+  providers.push(provider(HelpProvider));
+  providers.push(provider(PromptProvider));
+  providers.push(provider(OverlayProvider));
+  return providers;
+};
