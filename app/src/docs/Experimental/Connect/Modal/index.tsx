6665a1a32f44bc7bbaabf1127e5dd23bba3b7255
Nikos Kontakis <wirednkod@gmail.com>
2023-11-16 00:42:28 +0200

    feat: bugs on Connect modal (and scrollbars) (#812)

--- app/src/docs/Experimental/Connect/Modal/index.tsx
+++ app/src/docs/Experimental/Connect/Modal/index.tsx
@@ -3,28 +3,10 @@ SPDX-License-Identifier: GPL-3.0-only */
 
 import { Doc } from "./main";
 
-import {
-  Connect,
-  connectInfo,
-  ConnectConfigProvider,
-} from "@packages/cloud-react/lib/recipes/Connect";
-import type { DappInfo } from "@packages/cloud-react/lib/recipes/Connect";
-
 export const ModalConnect = () => {
-  const dappInfo: DappInfo = {
-    dappName: "dApp Name",
-    network: "polkadot",
-    ss58: 0,
-  };
-  const providers = connectInfo(dappInfo);
-
   return (
-    <ConnectConfigProvider dappInfo={dappInfo}>
-      <Connect providers={providers}>
-        <div className="doc">
-          <Doc npm="@polkadot-cloud/react" folder="Recipes/ModalConnect" />
-        </div>
-      </Connect>
-    </ConnectConfigProvider>
+    <div className="doc">
+      <Doc npm="@polkadot-cloud/react" folder="Recipes/ModalConnect" />
+    </div>
   );
 };
--- app/src/docs/Experimental/Connect/Modal/main.tsx
+++ app/src/docs/Experimental/Connect/Modal/main.tsx
@@ -7,7 +7,34 @@ import { DocProps } from "@docs/types";
 import { ModalConnect } from "./ModalConnect";
 import { Note } from "@docs/Note";
 
+import {
+  Connect,
+  connectInfo,
+  ConnectConfigProvider,
+} from "@packages/cloud-react/lib/recipes/Connect";
+import type {
+  ConnectType,
+  DappInfo,
+} from "@packages/cloud-react/lib/recipes/Connect";
+import { ModalConnectWalletSettings } from "./ModalConnectWalletSettings";
+
+const walletSettings: ConnectType = {
+  hardwareActive: false, // shows hardware wallets | defaults to true
+  webActive: true, // shows web wallets | defaults to true
+  devActive: true, // shows developer tools | defaults to true
+  readOnlyActive: false, // shows readonly tab | defaults to true
+  proxiesActive: true, // // shows proxies tab | defaults to true
+};
+
 export const Doc = ({ folder, npm }: DocProps) => {
+  const dappInfo: DappInfo = {
+    dappName: "dApp Name",
+    network: "polkadot",
+    ss58: 0,
+  };
+
+  const providers = connectInfo(dappInfo, walletSettings);
+
   return (
     <>
       <Edit folder={folder} />
@@ -44,7 +71,36 @@ export const Doc = ({ folder, npm }: DocProps) => {
 
       <p>The settings that need to be addded</p>
 
-      <ModalConnect />
+      <ConnectConfigProvider dappInfo={dappInfo}>
+        <Connect providers={providers}>
+          <ModalConnect />
+        </Connect>
+      </ConnectConfigProvider>
+
+      <p>
+        The <code>'wallets'</code> prop receive 5 different booleans that can
+        show/hide different parts of the modal (hardware/web/dev wallets, read
+        only and proxy accounts), and this way it is more adjustable to the
+        project's needs.
+      </p>
+      <p>
+        The example below shows only the web wallets, the developer tools active
+        and the proxies accounts active, while hiding the hardware wallets and
+        the read only accounts.
+      </p>
+
+      <Note>
+        <p>
+          <code>Button</code> code remains the same as above, so its not shown
+          here for reducing repeatance.
+        </p>
+      </Note>
+
+      <ConnectConfigProvider dappInfo={dappInfo} wallets={walletSettings}>
+        <Connect providers={providers}>
+          <ModalConnectWalletSettings />
+        </Connect>
+      </ConnectConfigProvider>
     </>
   );
 };
